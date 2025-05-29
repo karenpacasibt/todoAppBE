47a5efb01fc97fcbb8d442d4d241962888c0be37
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-05-29 18:20:24 -0400

    auth

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { verifyToken } = require('../middlewares/auth.middleware');
 const categoryController = require('../controllers/category.controller');
 
-router.get('/', categoryController.getAll);
-router.get('/:id', categoryController.getOne);
-router.post('/', categoryController.create);
-router.put('/:id', categoryController.update);
-router.delete('/:id', categoryController.destroy);
+
+router.get('/', verifyToken, categoryController.getAll);
+router.get('/:id', verifyToken, categoryController.getOne);
+router.post('/', verifyToken, categoryController.create);
+router.put('/:id', verifyToken, categoryController.update);
+router.delete('/:id', verifyToken, categoryController.destroy);
 
 module.exports = router;
